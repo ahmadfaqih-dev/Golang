9476d9cf8011348dab3aba00e4b70f9699a93e00
ahmadfaqih-dev <faqih.elekyoben@gmail.com>
2020-09-20 17:08:48 +0700

    conversion string to integer

--- converstion.go
+++ converstion.go
@@ -1,10 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	var x int32 = 10
 	var y int64 = int64(x)
 
-	fmt.Print(y)
+	fmt.Println(y)
+
+	var z float64 = float64(y)
+
+	fmt.Println(z)
+
+	var b float64 = 3.3
+
+	var a int32 = int32(b)
+
+	fmt.Println(a)
+
+//	conversi string ke integer
+
+	var nilai string = "100"
+
+	nilaiInt, _ := strconv.Atoi(nilai)
+
+	fmt.Println(nilaiInt)
+
+//	conversion integer to string
+
+	nilaiString := strconv.Itoa(-100)
+
+	fmt.Println(nilaiString)
+
+	nilaiBool, _ := strconv.ParseBool("true")
+
+	fmt.Println(nilaiBool)
 }
