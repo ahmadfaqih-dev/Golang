7327b665f1c130a3b72d5323664d7b5750a17191
ahmadfaqih-dev <faqih.elekyoben@gmail.com>
2020-09-23 06:43:13 +0700

    comparator atau perbandingan

---
+++ comparator.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 3
+	b := 1
+
+	// == (sama dengan) != (tdk sama dgn) a < b (kurang dari) a > b (lebih dari) a <= b (kurang dari sama dengan)
+	result := a <= b
+
+
+	fmt.Println(result)
+}
