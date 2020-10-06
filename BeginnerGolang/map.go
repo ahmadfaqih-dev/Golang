d446ed4dff45c83ba20a58d943baa5d22e4cd4b8
ahmadfaqih-dev <faqih.elekyoben@gmail.com>
2020-10-06 08:02:49 +0700

    map

---
+++ BeginnerGolang/map.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	 mahasiswa := make(map[string]string)
+
+	 mahasiswa["1001"] = "Ahmad"
+	 mahasiswa["1002"] = "Faqih"
+	 mahasiswa["1003"] = "Nazafarin"
+
+	 fmt.Println(mahasiswa)
+	 fmt.Println(mahasiswa["1001"])
+	 fmt.Println(mahasiswa["1002"])
+	 fmt.Println(mahasiswa["1003"])
+
+	for nim, name := range mahasiswa {
+		fmt.Println("Nim", nim, "Bernama", name)
+	}
+}
